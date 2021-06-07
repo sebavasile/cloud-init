f183e53fcdc50f8036affdbf121ef910d214781b
sebavasile <sebavasile@gmail.com>
2021-06-07 17:05:06 +0300

    adjusted read and initial wget command in order to properly promt for credentials for Dockerhub

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wget -O - -o /dev/null https://raw.githubusercontent.com/sebavasile/cloud-init/master/init.sh | bash
+# bash -c "$(wget -q -O - https://raw.githubusercontent.com/sebavasile/cloud-init/master/init.sh)"
 sudo timedatectl set-timezone UTC
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
@@ -20,12 +20,13 @@ sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 apt-get install -y git
 mkdir -p /app/ && cd /app
-echo "Enter credentials to authenticate to Docker Hub\n\n"
-echo "Username: "
-read DOCKER_USERNAME
-echo "Password: "
-read DOCKER_PASSWORD
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
+echo "Enter credentials to authenticate to Docker Hub"
+read -p "Username: " DOCKER_USERNAME
+read -s -p "Password: " DOCKER_PASSWORD
+echo ""
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
 echo "Clone Github repository. Enter Github credentials if prompted\n"
 git clone https://github.com/sebavasile/binance-updown-tokens-spread.git /app
 mkdir -p /app/python-project/stats
